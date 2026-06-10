9116c03c4d8226a1c8369853d27c33354b6ffd58
Redmer Kronemeijer <12477216+redmer@users.noreply.github.com>
2026-06-10 16:50:22 +0200

    feat: enhance error handling and diagnostics display in App and Page components

--- frontend/src/components/PageFooter.tsx
+++ frontend/src/components/PageFooter.tsx
@@ -5,6 +5,9 @@ import label from "../util/lang.js";
 export function PageFooter(props: { diagnostics?: string; repo?: string }) {
   const repo = props.repo ?? `https://github.com/redmer/link-beoordeelaar`;
   const session = useContext(ClientSessionContext);
+  const diagnostics = !!props.diagnostics
+    ? props.diagnostics
+    : JSON.stringify(session, undefined, 2);
   return (
     <footer className="diagnostics">
       <div>
@@ -15,7 +18,7 @@ export function PageFooter(props: { diagnostics?: string; repo?: string }) {
       <details className="">
         <summary>{label("DIAGNOSTICS_TITLE")}</summary>
         <code id="debug-diagnostics" className="overflow-x-scroll">
-          <pre>{JSON.stringify(session, undefined, 2)}</pre>
+          <pre>{diagnostics}</pre>
         </code>
       </details>
     </footer>
